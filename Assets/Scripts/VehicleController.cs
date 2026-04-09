d1d84ef8905b59e575b0cd15dd9d041f9c434b9d
bhavya-212 <bhavya.anil2023@gmail.com>
2026-04-08 19:13:12 -0700

    added obstacle collision

--- Assets/Scripts/VehicleController.cs
+++ Assets/Scripts/VehicleController.cs
@@ -16,6 +16,11 @@ public class VehicleController : MonoBehaviour
     void Update()
     {
         GetComponent<Rigidbody>().AddRelativeForce(new Vector3(desired_acceleration * power, 0, 0));
+        float dx = (Mouse.current.position.x.value - Screen.width / 2) / 200;
+        if (Mathf.Abs(dx) > 0.01f)
+        {
+            transform.Rotate(0, dx, 0);
+        }
     }
 
     void OnMove(InputValue value)
